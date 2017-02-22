ce648bca402fedd563d6da14fcaaf1309d07622a
547 <sevenwang@SevenWangdeiMac.local>
2017-02-22 16:12:46 -0700

    打包成Framework
    用carthage打包成Framework

---
+++ Carthage/Build/iOS/GUITabPagerViewController.framework/Headers/GUITabPagerViewControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  GUITabPagerViewControllerFramework.h
+//  GUITabPagerViewControllerFramework
+//
+//  Created by SevenWang on 2017/2/22.
+//  Copyright © 2017年 SevenWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GUITabPagerViewControllerFramework.
+FOUNDATION_EXPORT double GUITabPagerViewControllerFrameworkVersionNumber;
+
+//! Project version string for GUITabPagerViewControllerFramework.
+FOUNDATION_EXPORT const unsigned char GUITabPagerViewControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GUITabPagerViewControllerFramework/PublicHeader.h>
+
+
